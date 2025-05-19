16636166fb3dd43b7d4995f6caa394f15c198d04
Eyad-waleed <58422391+Eyad-waleed@users.noreply.github.com>
2025-05-19 23:59:32 +0300

    Delete epic_games_bot_updated/epic_games_bot directory

--- epic_games_bot_updated/epic_games_bot/epic_games_bot/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Epic Games Freebie Auto-Claimer Bot
------------------------------------
-A bot that automatically claims free games from Epic Games Store
-and sends notifications via Telegram and/or Discord.
-"""
-
-__version__ = "1.0.0"
